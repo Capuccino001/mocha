8855e74fc282c703b10930e6661b2976c11cf5b4
Capuccino <168435552+Capuccino001@users.noreply.github.com>
2024-07-05 05:31:40 +0800

    Update cmdmart.js

--- scripts/cmds/cmdmart.js
+++ scripts/cmds/cmdmart.js
@@ -3,22 +3,22 @@ const axios = require("axios");
 module.exports = {
   config: {
     name: "cmdmart",
-    aliases: ["market"],
+    aliases: ["gm","goatmart","mart"],
     shortDescription: {
-      en: "View items available in the goatmart."
+      en: "View items available in the Goatmart"
     },
-    category: "market",
+    category: "goatstore",
     usage: "〖 Goatmart 〗\n━━━━━━━━━━━━━━━\n👑 Available Choices:\n-> ${event.body} page < page number >\n-> ${event.body} code < item ID >\n-> ${event.body} author < name >\n-> ${event.body} show < item ID >\n-> ${event.body} search < item name >\n-> ${event.body} edit < item ID >\n-> ${event.body} upload < item details in JSON format >",
     version: "10.5",
     role: 0,
-    author: "LiANE | ArYAN",
+    author: "ArYAN",
   },
   onStart: async ({ api, event, args, message }) => {
-    const serverURL = "https://official-goatmart.onrender.com";
+    const serverURL = "https://goat-mart-api.onrender.com";
 
     try {
       if (!args[0]) {
-        api.sendMessage(`〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁 〗\n━━━━━━━━━━━━━━━\n👑 Available Choices:\n-> ${event.body} page < page number >\n-> ${event.body} author < name >\n-> ${event.body} code < item ID >\n-> ${event.body} show < item ID >\n-> ${event.body} search < item name >\n-> ${event.body} edit < item ID >\n-> ${event.body} upload < item details in JSON format >`, event.threadID, event.messageID);
+        api.sendMessage(`〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁  〗\n━━━━━━━━━━━━━━━\n👑 Available Choices:\n-> ${event.body} page < page number >\n-> ${event.body} author < name >\n-> ${event.body} code < item ID >\n-> ${event.body} show < item ID >\n-> ${event.body} search < item name >\n-> ${event.body} edit < item ID >\n-> ${event.body} upload < item details in JSON format >`, event.threadID, event.messageID);
       } else if (args[0] === "code") {
         const itemID = isNaN(args[1]) ? args[1] : parseInt(args[1]);
         const response = await axios.get(`${serverURL}/api/items/${itemID}`);
@@ -26,9 +26,9 @@ module.exports = {
         const codeExtracted = codeX.data;
 
         if (codeExtracted) {
-          message.reply(`〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁 〗\n━━━━━━━━━━━━━━━\n👑 𝗜𝘁𝗲𝗺 𝗡𝗮𝗺𝗲: ${response.data.itemName}\n🆔 𝗜𝘁𝗲𝗺 𝗜𝗗: ${response.data.itemID}\n⚙️ 𝗜𝘁𝗲𝗺 𝗧𝘆𝗽𝗲: ${response.data.type || 'Unknown' }\n💻 𝗔𝘂𝘁𝗵𝗼𝗿: ${response.data.authorName}\n📅 𝗔𝗱𝗱𝗲𝗱 𝗼𝗻: ${new Date(response.data.timestamp).toLocaleString()}\n✅ 𝗜𝘁𝗲𝗺 𝗖𝗼𝗱𝗲\n━━━━━━━━━━━━━━━\n${codeExtracted }`);
+          message.reply(`〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁  〗\n━━━━━━━━━━━━━━━\n👑 𝗜𝘁𝗲𝗺 𝗡𝗮𝗺𝗲: ${response.data.itemName}\n🆔 𝗜𝘁𝗲𝗺 𝗜𝗗: ${response.data.itemID}\n⚙ 𝗜𝘁𝗲𝗺 𝗧𝘆𝗽𝗲: ${response.data.type || 'Unknown' }\n💻 𝗔𝘂𝘁𝗵𝗼𝗿: ${response.data.authorName}\n📅 𝗔𝗱𝗱𝗲𝗱 𝗼𝗻: ${new Date(response.data.timestamp).toLocaleString()}\n✅ 𝗜𝘁𝗲𝗺 𝗖𝗼𝗱𝗲\n━━━━━━━━━━━━━━━\n${codeExtracted }`);
         } else {
-          api.sendMessage("〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁 〗\n━━━━━━━━━━━━━━━\nItem not found.", event.threadID, event.messageID);
+          api.sendMessage("〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁  〗\n━━━━━━━━━━━━━━━\nItem not found.", event.threadID, event.messageID);
         }
       } else if (args[0] === "author") {
         const authorName = args[1];
@@ -40,37 +40,37 @@ module.exports = {
             (item) =>
               `\n👑 𝗜𝘁𝗲𝗺 𝗡𝗮𝗺𝗲: ${item.itemName}
 🆔 𝗜𝘁𝗲𝗺 𝗜𝗗 : ${item.itemID}
-⚙️ 𝗜𝘁𝗲𝗺 𝗧𝘆𝗽𝗲: ${item.type || "Unknown"}
+⚙ 𝗜𝘁𝗲𝗺 𝗧𝘆𝗽𝗲: ${item.type || "Unknown"}
 📝 𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: ${item.description}
 📅 𝗔𝗱𝗱𝗲𝗱 𝗢𝗻: ${new Date(item.timestamp).toLocaleString()}\n\n━━━━━━━━━━━━━━━
 `
           );
           const itemInfo = itemDescriptions.join("\n");
 
-          message.reply(`〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁 〗\n━━━━━━━━━━━━━━━\n\n𝗜𝘁𝗲𝗺𝘀 𝗕𝘆: ${authorName}\n\n${itemInfo}`);
+          message.reply(`〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁  〗\n━━━━━━━━━━━━━━━\n\n𝗜𝘁𝗲𝗺𝘀 𝗕𝘆: ${authorName}\n\n${itemInfo}`);
         } else {
-          api.sendMessage(`〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁 〗\n━━━━━━━━━━━━━━━\n\nNo items found for this author.`, message.threadID);
+          api.sendMessage(`〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁  〗\n━━━━━━━━━━━━━━━\n\nNo items found for this author.`, message.threadID);
         }
       } else if (args[0] === "page") {
         const pageNumber = parseInt(args[1]);
         const response = await axios.get(`${serverURL}/api/items`);
         const items = response.data;
-        const totalPages = Math.ceil(items.length / 5);
-        const offset = (pageNumber - 1) * 5;
+        const totalPages = Math.ceil(items.length / 9);
+        const offset = (pageNumber - 1) * 9;
 
         if (pageNumber <= 0 || pageNumber > totalPages || isNaN(pageNumber)) {
-          api.sendMessage("〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁 〗\n━━━━━━━━━━━━━━━\nInvalid page number.", event.threadID, event.messageID);
+          api.sendMessage("〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁  〗\n━━━━━━━━━━━━━━━\nInvalid page number.", event.threadID, event.messageID);
         } else {
-          const pageItems = items.slice(offset, offset + 5);
+          const pageItems = items.slice(offset, offset + 9);
 
           const itemDescriptions = pageItems.map(
             (item) =>
-              `👑 𝗜𝘁𝗲𝗺 𝗡𝗮𝗺𝗲: ${item.itemName}\n🆔 𝗜𝘁𝗲𝗺 𝗜𝗗: ${item.itemID}\n⚙️ 𝗜𝘁𝗲𝗺 𝗧𝘆𝗽𝗲: ${item.type || "Unknown"}\n📝 𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: ${item.description}\n💻 𝗔𝘂𝘁𝗵𝗼𝗿: ${item.authorName}\n📅 𝗔𝗱𝗱𝗲𝗱 𝗢𝗻: ${new Date(item.timestamp).toLocaleString()}\n\n━━━━━━━━━━━━━━━\n`
+              `👑 𝗜𝘁𝗲𝗺 𝗡𝗮𝗺𝗲: ${item.itemName}\n🆔 𝗜𝘁𝗲𝗺 𝗜𝗗: ${item.itemID}\n⚙ 𝗜𝘁𝗲𝗺 𝗧𝘆𝗽𝗲: ${item.type || "Unknown"}\n📝 𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: ${item.description}\n💻 𝗔𝘂𝘁𝗵𝗼𝗿: ${item.authorName}\n📅 𝗔𝗱𝗱𝗲𝗱 𝗢𝗻: ${new Date(item.timestamp).toLocaleString()}\n\n━━━━━━━━━━━━━━━\n`
           );
 
           const itemInfo = itemDescriptions.join("\n");
 
-          message.reply(`〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁 〗\n━━━━━━━━━━━━━━━\nItems available in the market:\n\n${itemInfo}📝 𝗨𝘀𝗲 ${event.body.split(" ")[0]} [ show | code ] <item id> to view pastebin link or code.\n\n👑 𝗣𝗮𝗴𝗲𝘀: [ ${pageNumber} / ${totalPages} ]`);
+          message.reply(`〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁 〗\n━━━━━━━━━━━━━━━\nItems available in the 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁:\n\n${itemInfo}📝 𝗨𝘀𝗲 ${event.body.split(" ")[0]} [ show | code ] <item id> to view pastebin link or code.\n\n👑 𝗣𝗮𝗴𝗲𝘀: [ ${pageNumber} / ${totalPages} ]`);
         }
       } else if (args[0] === "show") {
         const itemID = isNaN(args[1]) ? args[1] : parseInt(args[1]); 
@@ -78,9 +78,9 @@ module.exports = {
         const item = response.data;
 
         if (item && itemID) {
-          message.reply(`〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁 〗\n━━━━━━━━━━━━━━━\n👑 𝗜𝘁𝗲𝗺 𝗡𝗮𝗺𝗲: ${item.itemName}\n🆔 𝗜𝘁𝗲𝗺 𝗜𝗗: ${item.itemID}\n📝 𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: ${item.description}\n🖇️ 𝗜𝘁𝗲𝗺 𝗟𝗶𝗻𝗸: ${item.pastebinLink}`);
+          message.reply(`〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁  〗\n━━━━━━━━━━━━━━━\n👑 𝗜𝘁𝗲𝗺 𝗡𝗮𝗺𝗲: ${item.itemName}\n🆔 𝗜𝘁𝗲𝗺 𝗜𝗗: ${item.itemID}\n📝 𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: ${item.description}\n🖇 𝗜𝘁𝗲𝗺 𝗟𝗶𝗻𝗸: ${item.pastebinLink}`);
         } else {
-          api.sendMessage("〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁 〗\n━━━━━━━━━━━━━━━\nItem not found.", event.threadID, event.messageID);
+          api.sendMessage("〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁  〗\n━━━━━━━━━━━━━━━\nItem not found.", event.threadID, event.messageID);
         }
       } else if (args[0] === "search") {
         const searchTerm = args.slice(1).join(" ").toLowerCase();
@@ -89,22 +89,22 @@ module.exports = {
         const matchingItems = items.filter(item => item.itemName.toLowerCase().includes(searchTerm) || item.description.toLowerCase().includes(searchTerm));
 
         if (matchingItems.length > 0) {
-          const itemDescriptions = matchingItems.map(item => `\n👑 𝗜𝘁𝗲𝗺 𝗡𝗮𝗺𝗲: ${item.itemName}\n🆔 𝗜𝘁𝗲𝗺 𝗜𝗗: ${item.itemID}\n⚙️ 𝗜𝘁𝗲𝗺 𝗧𝘆𝗽𝗲: ${item.type || "Unknown"}\n📝 𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: ${item.description}\n💻 𝗔𝘂𝘁𝗵𝗼𝗿: ${item.authorName}\n📅 𝗔𝗱𝗱𝗲𝗱 𝗢𝗻: ${new Date(item.timestamp).toLocaleString()}\n━━━━━━━━━━━━━━━\n`);
+          const itemDescriptions = matchingItems.map(item => `\n👑 𝗜𝘁𝗲𝗺 𝗡𝗮𝗺𝗲: ${item.itemName}\n🆔 𝗜𝘁𝗲𝗺 𝗜𝗗: ${item.itemID}\n⚙ 𝗜𝘁𝗲𝗺 𝗧𝘆𝗽𝗲: ${item.type || "Unknown"}\n📝 𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: ${item.description}\n💻 𝗔𝘂𝘁𝗵𝗼𝗿: ${item.authorName}\n📅 𝗔𝗱𝗱𝗲𝗱 𝗢𝗻: ${new Date(item.timestamp).toLocaleString()}\n━━━━━━━━━━━━━━━\n`);
           const itemInfo = itemDescriptions.join("\n");
 
-          message.reply(`〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁 〗\n━━━━━━━━━━━━━━━\n\n✅ Search Results for ${searchTerm}\n\n${itemInfo}`);
+          message.reply(`〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁  〗\n━━━━━━━━━━━━━━━\n\n✅ Search Results for ${searchTerm}\n\n${itemInfo}`);
         } else {
-          api.sendMessage("〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁 〗\n━━━━━━━━━━━━━━━\n\nNo items found matching the search term.", event.threadID, event.messageID);
+          api.sendMessage("〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁  〗\n━━━━━━━━━━━━━━━\n\nNo matching items found in goatmart.", event.threadID, event.messageID);
         }
       } else if (args[0] === "edit") {
         const itemID = isNaN(args[1]) ? args[1] : parseInt(args[1]); 
         const newItemDetails = JSON.parse(args.slice(2).join(" "));
         const response = await axios.put(`${serverURL}/api/items/${itemID}`, newItemDetails);
-        message.reply(`〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁 〗\n━━━━━━━━━━━━━━━\n\n✅ Item edited successfully\n👑 𝗜𝘁𝗲𝗺 𝗡𝗮𝗺𝗲: ${response.data.itemName}\n🆔 𝗜𝘁𝗲𝗺 𝗜𝗗: ${response.data.itemID}`);
+        message.reply(`〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁  〗\n━━━━━━━━━━━━━━━\n\n✅ Item edited successfully\n👑 𝗜𝘁𝗲𝗺 𝗡𝗮𝗺𝗲: ${response.data.itemName}\n🆔 𝗜𝘁𝗲𝗺 𝗜𝗗: ${response.data.itemID}`);
       } else if (args[0] === "upload") {
         const itemDetails = JSON.parse(args.slice(1).join(" "));
         const response = await axios.post(`${serverURL}/api/items`, itemDetails);
-        message.reply(`〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁 〗\n━━━━━━━━━━━━━━━\n\n✅ Item uploaded successfully\n👑 𝗜𝘁𝗲𝗺 𝗡𝗮𝗺𝗲: ${response.data.itemName}\n🆔 𝗜𝘁𝗲𝗺 𝗜𝗗: ${response.data.itemID}\n⚙️ 𝗜𝘁𝗲𝗺 𝗧𝘆𝗽𝗲: ${response.data.type || "Unknown"}`);
+        message.reply(`〖 𝗚𝗼𝗮𝘁𝗠𝗮𝗿𝘁  〗\n━━━━━━━━━━━━━━━\n\n✅ Item uploaded successfully\n👑 𝗜𝘁𝗲𝗺 𝗡𝗮𝗺𝗲: ${response.data.itemName}\n🆔 𝗜𝘁𝗲𝗺 𝗜𝗗: ${response.data.itemID}\n⚙ 𝗜𝘁𝗲𝗺 𝗧𝘆𝗽𝗲: ${response.data.type || "Unknown"}`);
       }
     } catch (error) {
       console.error("Error fetching items:", error);
