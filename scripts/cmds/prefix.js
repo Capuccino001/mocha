02ee113c18215c4f0041f00933a21e77ced970ae
Capuccino <168435552+Capuccino001@users.noreply.github.com>
2024-07-10 14:07:16 +0800

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -3,7 +3,7 @@ const { utils } = global;
 module.exports = {
   config: {
     name: "prefix",
-    alias: [""], 
+    alias: ["🧋"], 
     version: "1.3",
     author: "NTKhang",
     countDown: 5,
@@ -28,7 +28,7 @@ module.exports = {
   },
 
   onChat: async function ({ event, message, getLang }) {
-    if (event.body && (event.body.toLowerCase() === "prefix" || event.body.toLowerCase() === ""))
+    if (event.body && (event.body.toLowerCase() === "prefix" || event.body.toLowerCase() === "🧋"))
       return () => {
         return message.reply(getLang("myPrefix", global.GoatBot.config.prefix, utils.getPrefix(event.threadID)));
       };
